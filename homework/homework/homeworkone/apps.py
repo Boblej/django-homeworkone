d7ca67f34ef775f77c72a5b6bd73569316dabf91
Boble <67345802+Boblej@users.noreply.github.com>
2024-04-13 00:38:16 +0300

    Delete homework/homework directory

--- homework/homework/homeworkone/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class HomeworkoneConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'homeworkone'
--- homework/homework/homeworkone/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-def product(request,):
-    return render(request, 'homeworkone.html',)
-
-def product_detail(request,):
-    return render(request, 'homeworkone2.html',)
